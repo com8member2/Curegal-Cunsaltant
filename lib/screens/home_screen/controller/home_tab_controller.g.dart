7b0f528a6a6007f3bba17c38cb04edbece4aff55
milan_vadgama <milan.gajjar951@gmail.com>
2023-06-12 18:17:20 +0530

    calendar view design partially remaining other that that it is complete.

---
+++ lib/screens/home_screen/controller/home_tab_controller.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'home_tab_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$allBookingsHash() => r'd029b5cf2dc86ca10b2349ace4c0cb0ed2b07521';
+
+/// See also [allBookings].
+@ProviderFor(allBookings)
+final allBookingsProvider = AutoDisposeFutureProvider<List<dynamic>>.internal(
+  allBookings,
+  name: r'allBookingsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$allBookingsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AllBookingsRef = AutoDisposeFutureProviderRef<List<dynamic>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
